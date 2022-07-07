d3918acf6e05450a4b5177c81564a3a5feb18630
Natalie <nataliemae263@gmail.com>
2022-07-07 18:34:19 -0500

    Oh yeah baby

--- petfax/models.py
+++ petfax/models.py
@@ -5,6 +5,6 @@ db = SQLAlchemy()
 class Fact(db.Model):
     tablename = 'facts'
 
-id = db.Column(db.Integer, primary_key = True)
-submitter = db.Column(db.String(250))
-fact = db.Column(db.Text)
+    id = db.Column(db.Integer, primary_key = True)
+    submitter = db.Column(db.String(250))
+    fact = db.Column(db.Text)
--- petfax/pet.py
+++ petfax/pet.py
@@ -4,8 +4,14 @@ import json
 pets = json.load(open('pets.json'))
 print(pets)
 
-bp = Blueprint('pet', __name__, url_prefix="/pets")
+bp = Blueprint('pet', __name__, url_prefix="/pet")
 
 @bp.route('/')
-def index():
-    return render_template('index.html', pets=pets) 
+def index(): 
+    return render_template('index.html', pet=pets)
+
+
+@bp.route('/petfax')
+def show(id):
+    pet = pets[id - 1]
+    return render_template('show.html', pet=pets) 
